1a5d0012a3a4b5af970b8e08b2a2b9cfd47527ee
Richard Dallaway <richard@dallaway.com>
2014-08-18 15:47:49 +0100

    HTML5 the blank default project
    Made index and default template HTML5 valid
    Removed setting of HTML5 in Boot, as that's now the default.
    Fixed copyright footer for 2014
    Switched to data-lift and data-lift-content-id (from class)
    Weird rendering on Chrome, fixed by changing the content span from 17 to 16 (random)
    Sorry. Lots of white space change.

--- scala_211/lift_blank/src/main/scala/bootstrap/liftweb/Boot.scala
+++ scala_211/lift_blank/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -11,7 +11,6 @@ import Loc._
 import net.liftmodules.JQueryModule
 import net.liftweb.http.js.jquery._
 
-
 /**
  * A class that's instantiated early and run.  It allows the application
  * to modify lift's environment
@@ -27,7 +26,7 @@ class Boot {
 
       // more complex because this menu allows anything in the
       // /static path to be visible
-      Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
+      Menu(Loc("Static", Link(List("static"), true, "/static/index"),
 	       "Static Content")))
 
     // set the sitemap.  Note if you don't want access control for
@@ -37,17 +36,13 @@ class Boot {
     //Show the spinny image when an Ajax call starts
     LiftRules.ajaxStart =
       Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-    
+
     // Make the spinny image go away when it ends
     LiftRules.ajaxEnd =
       Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
 
     // Force the request to be UTF-8
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
-
-    // Use HTML5 for rendering
-    LiftRules.htmlProperties.default.set((r: Req) =>
-      new Html5Properties(r.userAgent))
 
     //Init the jQuery module, see http://liftweb.net/jquery for more information.
     LiftRules.jsArtifacts = JQueryArtifacts
